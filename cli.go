d515a1a879650617108d5ae58b285fd7647351ff
Jay Choo <jaychoo@gmail.com>
2015-07-18 14:43:19 -0400

    add command, cli skeleton

---
+++ cli.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// Run invokes the CLI with given arguments
+func Run(args []string) {
+
+	fmt.Println(args)
+}
--- command/converter.go
+++ command/converter.go
@@ -1,4 +1,4 @@
-package rdfgo
+package command
 
 import (
 	"fmt"
